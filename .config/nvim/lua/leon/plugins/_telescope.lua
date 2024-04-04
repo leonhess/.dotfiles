f46a318e396fb6a6dbf218034fdf4b3f20a80d68
leonhess <72307776+leonhess@users.noreply.github.com>
2024-04-04 18:11:29 +0200

    (NEOVIM:TELESCOPE) Actions imported, shortkeys

--- .config/nvim/lua/leon/plugins/_telescope.lua
+++ .config/nvim/lua/leon/plugins/_telescope.lua
@@ -1,4 +1,7 @@
-require('telescope').setup{
+local actions = require('telescope.actions')
+
+
+require('telescope').setup({
   defaults = {
     -- Default configuration for telescope goes here:
     -- config_key = value,
@@ -7,9 +10,11 @@ require('telescope').setup{
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["<C-h>"] = "which_key"
-      }
-    }
+        ['<ESC>'] = actions.close,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+      },
+    },
   },
   pickers = {
     -- Default configuration for builtin pickers goes here:
@@ -27,4 +32,12 @@ require('telescope').setup{
     -- }
     -- please take a look at the readme of the extension you want to configure
   }
-}
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>fd','<CMD>Telescope find_files search_dirs={"~HOME/.dotfiles/.config/nvim"}<CR>', {})
