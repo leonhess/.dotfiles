ef9d2726ef027d1b4481dbf5c7e59f9928524dd3
Leon Heß <leonhess423@gmail.com>
2026-09-08 14:01:30 +0200

    (NVIM:LSP) Lua lsp basic setup

--- .config/nvim/lua/leon/plugins/_lsp.lua
+++ .config/nvim/lua/leon/plugins/_lsp.lua
@@ -1,10 +1,13 @@
 
 local ruff_lsp = require('lsp.ruff')
+local lua_lsp = require('lsp.lua')
 
 local lsp_configs = {}
 
 -- Configure lsp config options
 vim.lsp.config("ruff", ruff_lsp)
+vim.lsp.config("lua", lua_lsp)
 
 -- Enable LSP config options
 vim.lsp.enable("ruff")
+vim.lsp.enable("lua")
